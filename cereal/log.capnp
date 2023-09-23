thatcobbyguy/openpilot

add nnffLog and startup alert

cereal/log.capnp
@@ -752,6 +752,7 @@ struct ControlsState @0x97ff69c53601abf1 {
     saturated @7 :Bool;
     actualLateralAccel @9 :Float32;
     desiredLateralAccel @10 :Float32;
+    nnffLog @11 :List(Float32);
    }
 
   struct LateralLQRState {

redmonBI/trailblazer

cereal/log.capnp
@@ -756,6 +756,7 @@ struct ControlsState @0x97ff69c53601abf1 {
     saturated @7 :Bool;
     actualLateralAccel @9 :Float32;
     desiredLateralAccel @10 :Float32;
+    nnLog @11 :List(Float32);
    }
 
   struct LateralLQRState {